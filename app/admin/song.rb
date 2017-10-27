cf72d9a75b6e1a53a8092b2ba036745864bcb318
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 20:01:45 +0000

    automatically backed up by learn

--- app/admin/song.rb
+++ app/admin/song.rb
@@ -1,17 +1,14 @@
 ActiveAdmin.register Song do
+
   permit_params :title, :artist_id
   actions :all, except: [:update, :edit, :new, :create, :destroy]
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+  form do |f|
+    inputs 'Song' do
+      f.input :title
+    end
+    f.semantic_errors
+    f.actions
+  end
 
 end
