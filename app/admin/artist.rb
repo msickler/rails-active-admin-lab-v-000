cf24b510cbbe2fb5356b92097b1509995f24d56b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 20:01:17 +0000

    automatically backed up by learn

--- app/admin/artist.rb
+++ app/admin/artist.rb
@@ -1,25 +1,14 @@
 ActiveAdmin.register Artist do
+
   permit_params :name
   actions :all, except: [:update, :edit, :new, :create, :destroy]
 
   form do |f|
-     inputs 'Artist' do
-       f.input :name
-     end
-     f.semantic_errors
-     f.actions
-   end
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+    inputs 'Artist' do
+      f.input :name
+    end
+    f.semantic_errors
+    f.actions
+  end
 
 end
