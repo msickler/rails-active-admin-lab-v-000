72b988cca896aae745b801c9a845f1b21adaf0f1
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 19:54:16 +0000

    automatically backed up by learn

--- app/admin/song.rb
+++ app/admin/song.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Song do
+  permit_params :title, :artist_id
   actions :all, except: [:update, :edit, :new, :create, :destroy]
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
